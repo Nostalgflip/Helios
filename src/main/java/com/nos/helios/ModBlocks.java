7c4ae0694a3a68ab25518c7ba02e4f6908c5280e
Nostalgflip <nostalgflip@gmail.com>
2017-02-13 21:46:15 +0100

    Forgot to push to git

--- src/main/java/com/nos/helios/ModBlocks.java
+++ src/main/java/com/nos/helios/ModBlocks.java
@@ -10,6 +10,7 @@ public class ModBlocks {
     public static IngotBlock solariteBlock;
     public static BasicBlock eboniteBlock;
     public static BasicBlock ivoriteBlock;
+    public static BasicBlock meteoriteBlock;
 
     public static BasicBlock machineCasing;
     public static MachineProjector machineProjector;
@@ -20,6 +21,7 @@ public class ModBlocks {
         solariteBlock = new IngotBlock("solarite_block");
         eboniteBlock = new IngotBlock("ebonite_block");
         ivoriteBlock = new IngotBlock("ivorite_block");
+        meteoriteBlock = new IngotBlock("meteorite_block");
 
         machineCasing = new BasicBlock(Material.IRON, "machine_casing");
         machineProjector = new MachineProjector();
@@ -32,6 +34,7 @@ public class ModBlocks {
         solariteBlock.initModel();
         eboniteBlock.initModel();
         ivoriteBlock.initModel();
+        meteoriteBlock.initModel();
 
         machineCasing.initModel();
         machineProjector.initModel();
---
+++ src/main/java/com/nos/helios/gui/testgui.java
@@ -0,0 +1,16 @@
+package com.nos.helios.gui;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+
+public class testgui extends GuiContainer {
+
+    public testgui(Container inventorySlotsIn) {
+        super(inventorySlotsIn);
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+
+    }
+}
